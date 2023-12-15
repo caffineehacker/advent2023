ee401c7185dba4bc49c5041401578c909451e1f2
Tim Waterhouse <tim@timwaterhouse.com>
2023-12-14 21:24:07 -0800

    Day 15, part 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -32,6 +32,59 @@ fn main() {
         .sum();
 
     println!("Part 1: {}", part1);
+
+    let mut boxes: Vec<Vec<(&str, usize)>> = Vec::new();
+    boxes.resize(256, Vec::new());
+    for operation in lines.get(0).unwrap().split(",") {
+        if operation.ends_with("-") {
+            let b = &mut boxes[hash(operation.trim_end_matches("-")) as usize];
+            for i in 0..b.len() {
+                if b[i].0 == operation.trim_end_matches("-") {
+                    b.remove(i);
+                    break;
+                }
+            }
+        } else {
+            let (name, value) = operation.split("=").collect_tuple().unwrap();
+            let value = value.parse::<usize>().unwrap();
+            let b = &mut boxes[hash(name) as usize];
+            let mut was_set = false;
+            for i in 0..b.len() {
+                if b[i].0 == name {
+                    b[i].1 = value;
+                    was_set = true;
+                    break;
+                }
+            }
+
+            if !was_set {
+                b.push((name, value));
+            }
+        }
+
+        if args.debug {
+            println!("After {}", operation);
+            for i in 0..boxes.len() {
+                if !boxes[i].is_empty() {
+                    println!("Box {}: {:?}", i, boxes[i]);
+                }
+            }
+        }
+    }
+
+    let part2 = boxes
+        .iter()
+        .enumerate()
+        .map(|(index, b)| {
+            (1 + index)
+                * b.iter()
+                    .enumerate()
+                    .map(|(lens_index, l)| (lens_index + 1) * l.1)
+                    .sum::<usize>()
+        })
+        .sum::<usize>();
+
+    println!("Part 2: {}", part2);
 }
 
 fn hash(text: &str) -> u64 {
