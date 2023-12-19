4de53a3093cf3bb57902c6260f5ef1215f6253df
Tim Waterhouse <tim@timwaterhouse.com>
2023-12-18 20:33:59 -0800

    Day 18 - part 2, using shoelace, not cleaned up

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -4,6 +4,7 @@ use std::{
     collections::{HashMap, HashSet},
     fs::File,
     io::{BufRead, BufReader},
+    iter,
 };
 
 #[derive(Parser, Debug)]
@@ -70,7 +71,7 @@ fn solve(instructions: Vec<(&str, i64)>, debug: bool) -> isize {
     let mut x = 0;
     let mut y = 0;
 
-    let mut dug = HashMap::new();
+    let mut dug = Vec::new();
 
     let mut previous_direction = (0, 0);
     for instruction in instructions.iter() {
@@ -82,141 +83,45 @@ fn solve(instructions: Vec<(&str, i64)>, debug: bool) -> isize {
             _ => panic!("Unexpected direction"),
         };
 
-        for _ in 0..instruction.1 {
-            dug.insert((x, y), (previous_direction, direction));
-            x += direction.0;
-            y += direction.1;
-            previous_direction = direction;
-        }
+        let new_x = x + direction.0 * instruction.1 as isize;
+        let new_y = y + direction.1 * instruction.1 as isize;
+        dug.push((
+            (x, y),
+            previous_direction,
+            direction,
+            instruction.1 as isize,
+        ));
+        previous_direction = direction;
+        x = new_x;
+        y = new_y;
     }
 
-    // We need to fill in the previous direction for the start
-    if dug.contains_key(&(-1, 0)) {
-        let potential = dug.get(&(-1, 0)).unwrap();
-        if potential.1 .0 == 1 {
-            dug.get_mut(&(0, 0)).unwrap().0 = potential.1;
-        }
-    }
-    if dug.contains_key(&(1, 0)) {
-        let potential = dug.get(&(1, 0)).unwrap();
-        if potential.1 .0 == -1 {
-            dug.get_mut(&(0, 0)).unwrap().0 = potential.1;
-        }
-    }
-    if dug.contains_key(&(0, -1)) {
-        let potential = dug.get(&(0, -1)).unwrap();
-        if potential.1 .1 == 1 {
-            dug.get_mut(&(0, 0)).unwrap().0 = potential.1;
-        }
-    }
-    if dug.contains_key(&(0, 1)) {
-        let potential = dug.get(&(0, 1)).unwrap();
-        if potential.1 .1 == -1 {
-            dug.get_mut(&(0, 0)).unwrap().0 = potential.1;
-        }
-    }
+    let before_origin = dug.iter().find(|entry| {
+        entry.0 .0 + entry.2 .0 * entry.3 == 0 && entry.0 .1 + entry.2 .1 * entry.3 == 0
+    });
+    dug[0].1 = before_origin.unwrap().2;
 
-    if debug {
-        println!("Trench: {}", dug.len());
-    }
-    let max_y = dug.keys().map(|k| k.1).max().unwrap();
-    let min_y = dug.keys().map(|k| k.1).min().unwrap();
+    // Shoelace method
+    let left = dug
+        .iter()
+        .tuple_windows()
+        .map(|(left, right)| left.0 .0 * right.0 .1)
+        .sum::<isize>()
+        + dug.last().unwrap().0 .0 * dug[0].0 .1;
+    let right = dug
+        .iter()
+        .tuple_windows()
+        .map(|(left, right)| left.0 .1 * right.0 .0)
+        .sum::<isize>()
+        + dug.last().unwrap().0 .1 * dug[0].0 .0;
+    let shoelace = (left - right).abs() / 2;
 
-    let mut row_cache = HashMap::new();
-    let mut solution = 0;
-    let mut y = min_y;
-    while y <= max_y {
-        let in_row = dug
-            .iter()
-            .filter(|((_, dy), _)| y == *dy)
-            .sorted_by_key(|((x, _), _)| *x)
-            .collect_vec();
-
-        let cache_key = in_row
-            .iter()
-            .map(|((x, _), directions)| (*x, **directions))
-            .collect_vec();
-
-        if row_cache.contains_key(&cache_key) {
-            solution += row_cache.get(&cache_key).unwrap();
-            if debug {
-                println!("Cache hit");
-            }
-            y += 1;
-            continue;
-        }
-
-        let mut inside = false;
-        let mut i = 0;
-        let mut row_score = 0;
-        while i < in_row.len() {
-            if debug {
-                println!("i: {}, {:?}, inside: {}", i, in_row[i], inside);
-            }
-            let cell = in_row[i];
-            if cell.1 .0 .1 != 0 && cell.1 .1 .1 != 0 {
-                // Vertical piece, we just count to the next piece no mater what it is
-                inside = !inside;
-                if inside == true {
-                    row_score += in_row[i + 1].0 .0 - cell.0 .0;
-                } else {
-                    row_score += 1;
-                }
-                i += 1;
-            } else if cell.1 .0 .1 != 0 && cell.1 .1 .0 != 0 {
-                // Turn from above or below
-                // We keep going until we get to a turn
-                i += 1;
-                row_score += 1;
-                while i + 1 < in_row.len() && in_row[i].0 .0 + 1 == in_row[i + 1].0 .0 {
-                    // Skip all of the consecutive pieces
-                    row_score += 1;
-                    i += 1;
-                }
-
-                // If the turn comes from the same direction then we are still inside / out, if from the opposite then we are switching
-                // We negate because one is a previous direction and one is next
-                if in_row[i].1 .1 .1 != -cell.1 .0 .1 {
-                    inside = !inside;
-                }
-                if inside {
-                    row_score += in_row[i + 1].0 .0 - in_row[i].0 .0 - 1;
-                }
-                row_score += 1;
-                i += 1;
-            } else if cell.1 .0 .0 != 0 && cell.1 .1 .1 != 0 {
-                // Turn to above or below
-                // We keep going until we get to a turn
-                i += 1;
-                row_score += 1;
-                while i + 1 < in_row.len() && in_row[i].0 .0 + 1 == in_row[i + 1].0 .0 {
-                    // Skip all of the consecutive pieces
-                    row_score += 1;
-                    i += 1;
-                }
-
-                // If the turn comes from the same direction then we are still inside / out, if from the opposite then we are switching
-                // We negate because one is a previous direction and one is next
-                if in_row[i].1 .0 .1 != -cell.1 .1 .1 {
-                    inside = !inside;
-                }
-                if inside {
-                    row_score += in_row[i + 1].0 .0 - in_row[i].0 .0 - 1;
-                }
-                row_score += 1;
-                i += 1;
-            }
-        }
-        row_cache.insert(cache_key, row_score);
-        solution += row_score;
-        if debug {
-            println!("y: {}, solution: {}", y, solution);
-        }
-
-        y += 1;
-    }
-
-    return solution;
+    // Pick's Theorom
+    let perimeter = dug
+        .iter()
+        .map(|(_, _, _, distance)| *distance)
+        .sum::<isize>();
+    return shoelace + perimeter / 2 + 1;
 }
 
 // Notes for tomorrow: We need to just record the lengths of everything and can key them to the min x and min y value to make everything easier
